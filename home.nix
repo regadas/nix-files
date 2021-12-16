3cedd8e9b520582d3a7d9fd6144c14bce301267a
Filipe Regadas <filiperegadas@gmail.com>
2021-12-16 15:48:43 +0000

    Relocate programs

--- home.nix
+++ home.nix
@@ -17,104 +17,101 @@ in {
 
   nixpkgs.config.allowUnfree = true;
 
-  programs.home-manager.enable = true;
-
-  programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-  };
-
-  programs.git = {
-    enable = true;
-    userName = "regadas";
-    userEmail = "filiperegadas@gmail.com";
-    signing = {
-      key = "2572CF0C";
-      signByDefault = true;
-    };
-    aliases = {
-      s = "status";
-      l =
-        "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-    };
-    extraConfig = {
-      rebase = { autoStash = true; };
-      pull = { rebase = true; };
-      hub = { host = "ghe.spotify.net"; };
-    };
-    ignores = [ ".java_version" ".metals" "metals.sbt" ".bloop" ".idea" ];
-  };
-
-  programs.tmux = {
-    enable = true;
-    terminal = "screen-256color";
-    baseIndex = 1;
-    keyMode = "vi";
-    shortcut = "s";
-
-    extraConfig = ''
-      # act like vim
-      bind-key h select-pane -L
-      bind-key j select-pane -D
-      bind-key k select-pane -U
-      bind-key l select-pane -R
-      bind-key -r C-h select-window -t :-
-      bind-key -r C-l select-window -t :+
-
-      # set -g prefix2 C-s
-      # renumber windows sequentially after closing any of them
-      set -g renumber-windows on
-
-
-      # soften status bar color from harsh green to light gray
-      set -g status-bg '#666666'
-      set -g status-fg '#aaaaaa'
-
-      # remove administrative debris (session name, hostname, time) in status bar
-      set -g status-left '''
-      set -g status-right '''
-
-      # prefix -> back-one-character
-      bind-key C-b send-prefix
-      # prefix-2 -> forward-incremental-history-search
-      bind-key C-s send-prefix -2
-
-      # don't suspend-client
-      unbind-key C-z
-    '';
-  };
-
-  programs.zsh = {
-    enable = true;
-    autocd = true;
-    enableAutosuggestions = true;
-    history.extended = true;
-    shellAliases = {
-      cat = "bat";
-      git = "hub";
-    };
-    plugins = [
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-      {
-        name = "powerlevel10k-config";
-        src = lib.cleanSource ./config/p10k;
-        file = "p10k.zsh";
-      }
-    ];
-  };
-
   #programs.zsh.initExtraBeforeCompInit =
   #      "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-  programs.direnv.enable = true;
-
-  programs.java.enable = true;
 
   programs = {
+    home-manager.enable = true;
+
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+
+    git = {
+      enable = true;
+      userName = "regadas";
+      userEmail = "filiperegadas@gmail.com";
+      signing = {
+        key = "2572CF0C";
+        signByDefault = true;
+      };
+      aliases = {
+        s = "status";
+        l =
+          "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      };
+      extraConfig = {
+        rebase = { autoStash = true; };
+        pull = { rebase = true; };
+        hub = { host = "ghe.spotify.net"; };
+      };
+      ignores = [ ".java_version" ".metals" "metals.sbt" ".bloop" ".idea" ];
+    };
+
+    tmux = {
+      enable = true;
+      terminal = "screen-256color";
+      baseIndex = 1;
+      keyMode = "vi";
+      shortcut = "s";
+
+      extraConfig = ''
+        # act like vim
+        bind-key h select-pane -L
+        bind-key j select-pane -D
+        bind-key k select-pane -U
+        bind-key l select-pane -R
+        bind-key -r C-h select-window -t :-
+        bind-key -r C-l select-window -t :+
+
+        # set -g prefix2 C-s
+        # renumber windows sequentially after closing any of them
+        set -g renumber-windows on
+
+
+        # soften status bar color from harsh green to light gray
+        set -g status-bg '#666666'
+        set -g status-fg '#aaaaaa'
+
+        # remove administrative debris (session name, hostname, time) in status bar
+        set -g status-left '''
+        set -g status-right '''
+
+        # prefix -> back-one-character
+        bind-key C-b send-prefix
+        # prefix-2 -> forward-incremental-history-search
+        bind-key C-s send-prefix -2
+
+        # don't suspend-client
+        unbind-key C-z
+      '';
+    };
+
+    zsh = {
+      enable = true;
+      autocd = true;
+      enableAutosuggestions = true;
+      history.extended = true;
+      shellAliases = {
+        cat = "bat";
+        git = "hub";
+      };
+      plugins = [
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+        {
+          name = "powerlevel10k-config";
+          src = lib.cleanSource ./config/p10k;
+          file = "p10k.zsh";
+        }
+      ];
+    };
+
     broot.enable = true;
 
     bat = {
@@ -131,6 +128,8 @@ in {
 
     alacritty.enable = true;
     vscode.enable = true;
+    java.enable = true;
+    direnv.enable = true;
   };
 
   home.packages = with pkgs; [
