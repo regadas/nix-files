b1c184d5f181575ce28d9b07c91dafa930df63b4
Filipe Regadas <filiperegadas@gmail.com>
2022-08-14 18:17:46 +0100

    Format

--- configuration.nix
+++ configuration.nix
@@ -3,15 +3,10 @@
 {
   # Nix configuration ------------------------------------------------------------------------------
 
-  nix.binaryCaches = [
-    "https://cache.nixos.org/"
-  ];
-  nix.binaryCachePublicKeys = [
-    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-  ];
-  nix.trustedUsers = [
-    "@admin"
-  ];
+  nix.binaryCaches = [ "https://cache.nixos.org/" ];
+  nix.binaryCachePublicKeys =
+    [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+  nix.trustedUsers = [ "@admin" ];
   users.nix.configureBuildUsers = true;
 
   # Enable experimental nix command and flakes
