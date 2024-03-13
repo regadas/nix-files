62bbb74fa2f3a1ce49e2cd11e4c2dc705601cb97
regadas <oss@regadas.email>
2024-03-13 22:34:04 +0000

    Update wezterm font

--- home.nix
+++ home.nix
@@ -192,7 +192,7 @@
       enable = true;
       extraConfig = ''
         return {
-          font = wezterm.font("Iosevka"),
+          font = wezterm.font("JetBrains Mono"),
           font_size = 16.0,
           color_scheme = "catppuccin-mocha",
           hide_tab_bar_if_only_one_tab = true,
