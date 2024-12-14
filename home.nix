dafe00a1f99229b7c2acbf35007eea46fe545ae7
regadas <oss@regadas.email>
2024-12-14 06:12:45 -0500

    Add python uv

--- home.nix
+++ home.nix
@@ -217,8 +217,28 @@
       package = pkgs.jdk21;
     };
 
-    direnv.enable = true;
-    direnv.nix-direnv.enable = true;
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+
+      stdlib = ''
+        layout_uv() {
+            if [[ -d ".venv" ]]; then
+                VIRTUAL_ENV="$(pwd)/.venv"
+            fi
+
+            if [[ -z $VIRTUAL_ENV || ! -d $VIRTUAL_ENV ]]; then
+                log_status "No virtual environment exists. Executing \`uv venv\` to create one."
+                uv venv
+                VIRTUAL_ENV="$(pwd)/.venv"
+            fi
+
+            PATH_add "$VIRTUAL_ENV/bin"
+            export UV_ACTIVE=1  # or VENV_ACTIVE=1
+            export VIRTUAL_ENV
+        }
+      '';
+    };
 
     texlive = {
       enable = true;
@@ -251,6 +271,7 @@
 
   home.packages = with pkgs;
     [
+      uv
       cmake
       ffmpegthumbnailer
       tree
