bfaffb08973b23dca0031db769073b370baa7717
regadas <oss@regadas.email>
2024-12-28 09:36:30 +0000

    Add new darwin host specific config

---
+++ hosts/darwin/default.nix
@@ -0,0 +1,51 @@
+{ self, pkgs, inputs, ... }:
+let inherit (inputs.nixpkgs-unstable.lib) attrValues optionalAttrs singleton;
+in {
+  # nixpkgs.overlays = attrValues self.overlays ++ singleton (
+  #   # Sub in x86 version of packages that don't build on Apple Silicon yet
+  #   final: prev:
+  #   (optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
+  #     inherit (final.pkgs-x86) nix-index niv purescript bazel;
+  #   }));
+
+  users.users.regadas.home = "/Users/regadas";
+
+  # Add ability to used TouchID for sudo authentication
+  security.pam.enableSudoTouchIdAuth = true;
+
+  services.yabai.enable = true;
+  services.yabai.package = pkgs.yabai;
+  services.skhd.enable = true;
+
+  launchd.user.agents.skhd.serviceConfig = {
+    StandardOutPath = "/tmp/skhd.out.log";
+    StandardErrorPath = "/tmp/skhd.err.log";
+  };
+  launchd.user.agents.yabai.serviceConfig = {
+    StandardOutPath = "/tmp/yabai.out.log";
+    StandardErrorPath = "/tmp/yabai.err.log";
+  };
+
+  # Apps
+  # `home-manager` currently has issues adding them to `~/Applications`
+  # Issue: https://github.com/nix-community/home-manager/issues/1341
+  environment.systemPackages = with pkgs; [ terminal-notifier ];
+
+  homebrew = {
+    enable = true;
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "uninstall";
+      upgrade = true;
+    };
+    brews = [
+      "zlib"
+      {
+        name = "emacs-plus@30";
+        args = [ "with-xwidgets" "with-native-comp" "with-imagemagick" ];
+      }
+    ];
+    taps = [ "d12frosted/emacs-plus" ];
+  };
+
+}
