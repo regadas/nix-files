f9b272329b6a44d6510363515c6c742e51180ee7
regadas <oss@regadas.email>
2024-12-30 10:48:18 +0000

    further rework and simplification of the flake.nix

--- flake.nix
+++ flake.nix
@@ -14,118 +14,25 @@
   };
 
   outputs = { darwin, home-manager, ... }@inputs:
-    let
-
-      inherit (darwin.lib) darwinSystem;
-      inherit (inputs.nixpkgs-unstable.lib) optionalAttrs;
-
-      # Main `nix-darwin` config
-      config = { pkgs, lib, ... }: {
-
-        nix.settings = {
-          trusted-users = [ "@admin" ];
-          substituters = [ "https://cache.nixos.org/" ];
-          trusted-public-keys = [
-            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-          ];
-        };
-        nix.configureBuildUsers = true;
-
-        # Enable experimental nix command and flakes
-        # nix.package = pkgs.nixUnstable;
-        nix.extraOptions = ''
-          auto-optimise-store = true
-          experimental-features = nix-command flakes
-        '' + lib.optionalString (pkgs.system == "aarch64-darwin") ''
-          extra-platforms = x86_64-darwin aarch64-darwin
-        '';
-
-        programs.fish.enable = true;
-
-        # Auto upgrade nix package and the daemon service.
-        services.nix-daemon.enable = true;
-
-        programs.nix-index.enable = true;
-
-        # Fonts
-        fonts.packages = with pkgs; [
-          recursive
-          nerd-fonts.iosevka
-          nerd-fonts.jetbrains-mono
-          nerd-fonts.fira-code
-          nerd-fonts.blex-mono
-          iosevka-bin
-          font-awesome
-          cascadia-code
-          fira-code
-          jetbrains-mono
-          ibm-plex
-          intel-one-mono
-        ];
-
-        system.stateVersion = 5;
-
-        ids.gids.nixbld = 30000;
-
-        # Keyboard
-        system.keyboard.enableKeyMapping = true;
-        system.keyboard.remapCapsLockToEscape = true;
-
-      };
-
+    let inherit (darwin.lib) darwinSystem;
     in {
       # My `nix-darwin` configs
       darwinConfigurations = {
         Filipes-MacBook-Air = darwinSystem {
           system = "x86_64-darwin";
           specialArgs = inputs;
-          modules = [
-            config
-            home-manager.darwinModules.home-manager
-            ./hosts/darwin
-          ];
+          modules = [ home-manager.darwinModules.home-manager ./hosts/darwin ];
         };
         MacPro = darwinSystem {
           system = "x86_64-darwin";
           specialArgs = inputs;
-          modules = [
-            config
-            home-manager.darwinModules.home-manager
-            ./hosts/darwin
-          ];
+          modules = [ home-manager.darwinModules.home-manager ./hosts/darwin ];
         };
         PN402PJ2C6 = darwinSystem {
           system = "aarch64-darwin";
           specialArgs = inputs;
-          modules = [
-            config
-            home-manager.darwinModules.home-manager
-            ./hosts/darwin
-          ];
+          modules = [ home-manager.darwinModules.home-manager ./hosts/darwin ];
         };
       };
-
-      # Overlays --------------------------------------------------------------- {{{
-
-      overlays = {
-        # Overlays to add various packages into package set
-        pkgs-unstable = final: prev: {
-          unstable = import inputs.nixpkgs-unstable {
-            inherit (prev.stdenv) system;
-            inherit config;
-          };
-        };
-
-        # Overlay useful on Macs with Apple Silicon
-        apple-silicon = final: prev:
-          optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-            # Add access to x86 packages system is running Apple Silicon
-            pkgs-x86 = import inputs.nixpkgs-unstable {
-              system = "x86_64-darwin";
-              inherit config;
-            };
-          };
-      };
-
     };
 }
--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -1,4 +1,52 @@
-{ }: {
+{ lib, pkgs, ... }: {
+
+  nix.settings = {
+    trusted-users = [ "@admin" ];
+    substituters = [ "https://cache.nixos.org/" ];
+    trusted-public-keys =
+      [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+  };
+  nix.configureBuildUsers = true;
+
+  # Enable experimental nix command and flakes
+  # nix.package = pkgs.nixUnstable;
+  nix.extraOptions = ''
+    auto-optimise-store = true
+    experimental-features = nix-command flakes
+  '' + lib.optionalString (pkgs.system == "aarch64-darwin") ''
+    extra-platforms = x86_64-darwin aarch64-darwin
+  '';
+
+  programs.fish.enable = true;
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+  programs.nix-index.enable = true;
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    recursive
+    nerd-fonts.iosevka
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.fira-code
+    nerd-fonts.blex-mono
+    iosevka-bin
+    font-awesome
+    cascadia-code
+    fira-code
+    jetbrains-mono
+    ibm-plex
+    intel-one-mono
+  ];
+
+  system.stateVersion = 5;
+
+  ids.gids.nixbld = 30000;
+
+  # Keyboard
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToEscape = true;
 
   nixpkgs = {
     config = {
