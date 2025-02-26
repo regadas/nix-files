941077694249637f8d54f8036fcb94aa0c038774
regadas <oss@regadas.email>
2025-02-26 20:02:58 +0000

    Update touchIdAuth property

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -5,7 +5,7 @@
   users.users.regadas.home = "/Users/regadas";
 
   # Add ability to used TouchID for sudo authentication
-  security.pam.enableSudoTouchIdAuth = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
 
   services.yabai.enable = true;
   services.yabai.package = pkgs.yabai;
