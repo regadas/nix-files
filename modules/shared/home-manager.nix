9ef79ca0093d718c90718e48e459a9ab9b20258c
regadas <oss@regadas.email>
2025-06-17 07:51:07 -0400

    Add atuin

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -272,6 +272,12 @@
         };
       };
     };
+
+    atuin = {
+      enable = true;
+      enableFishIntegration = true;
+      daemon.enable = true;
+    };
   };
 
   home.packages = with pkgs;
