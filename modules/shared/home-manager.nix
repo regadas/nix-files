65292eeea357bda0202022ccddae1f145dcca9b7
regadas <oss@regadas.email>
2026-02-06 16:10:46 -0500

    Add mdopen from cargo

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -313,6 +313,23 @@
 
   home.packages = with pkgs;
     [
+      (rustPlatform.buildRustPackage rec {
+        pname = "mdopen";
+        version = "0.5.0";
+        src = fetchFromGitHub {
+          owner = "immanelg";
+          repo = "mdopen";
+          rev = "3858a4d2222ac789b168729ac1ae7b726342526e";
+          hash = "sha256-a0yhi0oRQTl3lIAkzar7hgCYM6gDX6YQ9Oc18/jYlwg=";
+        };
+        cargoHash = "sha256-fr7gd61NxKjuYUFePPx4qil2owzfCF6kDMlOga4AfCE=";
+        doCheck = false;
+        meta = {
+          description = "Preview markdown files in a browser";
+          homepage = "https://github.com/immanelg/mdopen";
+          license = lib.licenses.bsd3;
+        };
+      })
       copilot-language-server
       obsidian
       sesh
