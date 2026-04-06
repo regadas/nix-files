732a51f75607b599dbe30d1c90ef793eb09cf70b
regadas <oss@regadas.email>
2026-04-06 14:27:47 +0100

    Update claude-agent-acp to v0.25.1

--- packages/claude-agent-acp.nix
+++ packages/claude-agent-acp.nix
@@ -2,14 +2,14 @@
 
 pkgs.buildNpmPackage rec {
   pname = "claude-agent-acp";
-  version = "0.24.2";
+  version = "0.25.1";
   src = pkgs.fetchFromGitHub {
     owner = "zed-industries";
     repo = "claude-agent-acp";
     rev = "v${version}";
-    hash = "sha256-SRVbLcGrH5pJt6yfM0ObSso68M+yGateIVYf/kFVDhE=";
+    hash = "sha256-OJit4mQxVqdMbohGCbmZfycCzZnjtDX5NblwJ7QBzL4=";
   };
-  npmDepsHash = "sha256-V5lBQNhpL+/Mok9bEVSOrrHSv9B9pXKJswcXW+QDnAs=";
+  npmDepsHash = "sha256-KDYRPlPgi9K9HjOIopkUcGnauq034otdIHm0gQ2PjsU=";
   meta = {
     description = "Zed's Claude Agent ACP bridge";
     homepage = "https://github.com/zed-industries/claude-agent-acp";
