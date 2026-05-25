e6f7b484266978359ff222feac3e0ece532ea4d5
regadas <oss@regadas.email>
2026-05-24 23:02:02 -0400

    Update claude-agent-acp

--- packages/claude-agent-acp.nix
+++ packages/claude-agent-acp.nix
@@ -2,14 +2,14 @@
 
 pkgs.buildNpmPackage rec {
   pname = "claude-agent-acp";
-  version = "0.36.1";
+  version = "0.37.0";
   src = pkgs.fetchFromGitHub {
     owner = "agentclientprotocol";
     repo = "claude-agent-acp";
     rev = "v${version}";
-    hash = "sha256-mtdmzKuZF1eEt8FKm+VYp9h9igfRzQPQ2HCbq+IRVDw=";
+    hash = "sha256-x3dJiEntNNeh4kmChi+FddUOlvtDD8nWwkHNtcH93HU=";
   };
-  npmDepsHash = "sha256-3lX2fAjVYWeFl9FAsSou30WOJ8st9lUdyt54hC4jN1E=";
+  npmDepsHash = "sha256-8V1yf32GRYmS0f4M9HXclkY7byFWozT+RjlXHkFFdoA=";
   meta = {
     description = "Claude Agent ACP bridge";
     homepage = "https://github.com/agentclientprotocol/claude-agent-acp";
