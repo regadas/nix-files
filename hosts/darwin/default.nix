b3a04e7bcbf918ce0e6cf3e4444fa535e10c5e5a
regadas <oss@regadas.email>
2026-08-16 14:22:14 +0100

    chore(packages): remove terminal-notifier from system profile

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -30,11 +30,6 @@
     StandardErrorPath = "/tmp/yabai.err.log";
   };
 
-  # Apps
-  # `home-manager` currently has issues adding them to `~/Applications`
-  # Issue: https://github.com/nix-community/home-manager/issues/1341
-  environment.systemPackages = with pkgs; [ terminal-notifier ];
-
   homebrew = {
     enable = true;
     onActivation = {
